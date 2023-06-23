1dd9d142e9946efc48908e0ab602d74707d7384b
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2023-06-23 17:33:43 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,9 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BMMapView",
-            dependencies: []
-//            ,
-//            resources: [.copy("BMMapView/Sources/BMMapView/MapView")]
+            dependencies: [],
+            resources: [.process("BMMapView.nib")]
         ),
         .testTarget(
             name: "BMMapViewTests",
