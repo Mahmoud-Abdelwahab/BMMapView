d1bf998c402b03e697d90d28c4dac9bd4d071765
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2023-06-23 14:55:42 +0300

    Update Contruct.swift

--- Sources/BMMapView/Contruct.swift
+++ Sources/BMMapView/Contruct.swift
@@ -30,9 +30,14 @@ public protocol BMMapInputType: AnyObject {
 }
 
 public struct BMAnnotation {
-    var coordinate: CLLocationCoordinate2D
-    var title: String?
-    var subtitle: String?
+    public var coordinate: CLLocationCoordinate2D
+    public var title: String?
+    public var subtitle: String?
+    public init(coordinate: CLLocationCoordinate2D, title: String? = nil, subtitle: String? = nil) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+    }
 }
 
 extension BMMapDelegate {
