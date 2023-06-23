80094446019f6f0a7b9405e0a66a5ce28c4c7980
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2023-06-23 14:10:48 +0300

    init

--- Sources/BMMapView/BMMapView.swift
+++
@@ -1,6 +0,0 @@
-public struct BMMapView {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/BMMapViewTests/BMMapViewTests.swift
+++ Tests/BMMapViewTests/BMMapViewTests.swift
@@ -6,6 +6,6 @@ final class BMMapViewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(BMMapView().text, "Hello, World!")
+       // XCTAssertEqual(BMMapView().text, "Hello, World!")
     }
 }
