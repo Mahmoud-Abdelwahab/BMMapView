5401e2127c2f1e910c7da2360a2298761f67d675
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2023-07-17 11:38:06 +0300

    fixnishing contruct

--- Sources/BMMapView/Contruct.swift
+++ Sources/BMMapView/Contruct.swift
@@ -2,34 +2,73 @@
 //  Contruct.swift
 //  AppleMapFramework
 //
-//  Created by Mahmoud Abdulwahab on 22/06/2023.
+//  Created by Mahmoud Abdulwahab on 29/06/2023.
 //
 
 import MapKit
 
 
 public protocol BMMapDelegate: AnyObject {
+    
     func didSelectAnnotation(_ annotation: BMAnnotation)
     func didDrageOnMap()
     func didTapOnCalloutView(_ annotation: BMAnnotation)
 }
 
 public protocol BMMapInputType: AnyObject {
-    func centerToLocation(_ location: CLLocation, regionRadius: CLLocationDistance)
-    func addAnnotation(_ annotation: BMAnnotation)
-    func addAnnotations(_ annotations: [BMAnnotation])
-    func setDefaultPinIcon(with icon: UIImage)
-    func shouldShowCalloutView(_ isShown: Bool)
-    func removeAnnotations(_ annotations: [BMAnnotation])
-    func removeAnnotation(_ annotation: BMAnnotation)
+    /// Center Map to specific location
+    /// - Parameters:
+    ///   - annotations: Location to center the map to
+    ///   - regionRadius: Zoom level
+    func centerToAnnotation(_ annotations: BMAnnotation, regionRadius: Double)
     
-    func selectMarker(_ marker: BMAnnotation)
-    func animateToCoordinate(_ coordinate: CLLocationCoordinate2D, withZoomLevel zoomLevel: Double)
-    func drawAnnotations(_ annotations: [MKAnnotation])
-    func fitAnnotationsOnScreen()
+    /// Change default marker icon
+    /// - Parameter icon: marker icon
+    func setDefaultPinIcon(with icon: UIImage)
+    
+    /// Add some annotation on the map
+    /// - Parameter annotations: annotations to be added . if you want only one annotation send it also as array
+    func addAnnotations(_ annotations: [BMAnnotation])
+    
+    /// Showing Callout view above the annotation
+    /// - Parameter isShown: boolean  value
+    func shouldShowCalloutView(_ isShown: Bool)
+    
+    /// Select specific marker on the map by  selecting specific annotation eg. from tableview cell or collectionview
+    /// - Parameters:
+    ///   - annotation: The selected annotation
+    ///   - regionRadius: Zoom level
+    func selectAnnotation(_ annotation: BMAnnotation, regionRadius: Double)
+    
+    /// Remove annotation from make
+    /// - Parameter annotations: List of annotations to be removed
+    func removeAnnotations(_ annotations: [BMAnnotation])
+    
+    /// Fit list of annotations on the entire screen
+    /// - Parameters:
+    ///   - annotations: List of annotations to be fitted on the entire screen
+    ///   - edgePadding: Setting Edge insets to dynamically set  canvas padding for the annotations it takes default
+    func fitAnnotationsInTheScreen(_ annotations: [BMAnnotation], edgePadding: UIEdgeInsets)
+    
+    /// Resetting scaled Annotations
+    func resetAnnotationScaling()
+    
+    
+    /// Scale Annotation icon
+    /// - Parameter annotation: annotation to be scaled
+    /// - Parameter selectedScale: The scaling factor for icon
+    func scaleAnnotation(_ annotation: BMAnnotation, selectedScale: CGFloat)
+    
+    
+    /// Move Camera position to specific annotation with zoom level
+    /// - Parameters:
+    ///   - annotation: target annotation
+    ///   - zoomLevel: zoom level
+    ///   - animated: move with animation
+    func animateToAnnotation(_ annotation: BMAnnotation, zoomLevel: Double?, animated: Bool)
 }
 
-public struct BMAnnotation {
+public class BMAnnotation: NSObject, MKAnnotation {
     public var coordinate: CLLocationCoordinate2D
     public var title: String?
     public var subtitle: String?
