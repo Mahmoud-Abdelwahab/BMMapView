45982406cd80027e9b62eaca5fa3c6ebdffe8540
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2023-07-17 11:40:15 +0300

    adding core implementations 🚀

--- Sources/BMMapView/MapView/BMMapView.swift
+++ Sources/BMMapView/MapView/BMMapView.swift
@@ -17,8 +17,13 @@ public class BMMapView: UIView {
     public weak var delegate: BMMapDelegate?
     private var defaultPinIcon: UIImage?
     private var canShowCallout: Bool?
-    
-    // MARK: - Init 
+    private var lastSelectedAnnotationView: MKAnnotationView?
+    private var lastScaledAnnotation: BMAnnotation?
+    private let reuseIdentifier = "BMMapViewCell"
+    public var zoomLevel: Double? {
+        getZoomLevel()
+    }
+    // MARK: - Init
     public override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -46,47 +51,48 @@ extension BMMapView {
 
 
 extension BMMapView: MKMapViewDelegate {
-   
+    
     // MARK: - MKMapViewDelegate
     
     public func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
         guard let annotation = view.annotation else { return }
+        
         let bmAnnotation = BMAnnotation(
             coordinate: annotation.coordinate,
             title: annotation.title ?? nil,
             subtitle: annotation.subtitle ?? nil)
-    
         delegate?.didSelectAnnotation(bmAnnotation)
+        
+        
     }
     
     public func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
         delegate?.didDrageOnMap()
     }
     
-    public func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-        if  let annotation = mapView.annotations.first(where: { !($0 is MKUserLocation) }) {
-            mapView.selectAnnotation(annotation, animated: true)
-        }
-    }
-    
     public func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "userLocation")
-
+        let annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        annotationView.isUserInteractionEnabled = true
+        
         if let canShowCallout = canShowCallout {
             annotationView.canShowCallout = canShowCallout
         }
+        
         if let defaultPinIcon = defaultPinIcon {
             annotationView.image = defaultPinIcon
+        } else {
+            annotationView.image = UIImage(systemName: "mappin.and.ellipse")
         }
         
         let button = AnnotationButton(type: .detailDisclosure)
         button.addTarget(self, action: #selector(pinAction(_:)), for: .touchUpInside)
-        annotationView.rightCalloutAccessoryView = button
         if let pointAnnotation = annotation as? MKPointAnnotation {
             button.annotation = pointAnnotation
         }
+        annotationView.rightCalloutAccessoryView = button
         return annotationView
     }
+    
     
     @objc private func pinAction(_ pinButton: AnnotationButton) {
         guard let annotationPointModel = pinButton.annotation else { return }
@@ -97,64 +103,89 @@ extension BMMapView: MKMapViewDelegate {
 
 
 extension BMMapView: BMMapInputType {
+    public func selectAnnotation(_ annotation: BMAnnotation, regionRadius: Double) {
+        removeAnnotations([annotation])
+        addAnnotations([annotation])
+        centerToAnnotation(annotation,
+                           regionRadius: regionRadius)
+    }
     
     public func addAnnotations(_ annotations: [BMAnnotation]) {
+        mapView.clearsContextBeforeDrawing = true
         annotations.forEach(addAnnotation)
     }
     
-    public func centerToLocation(_ location: CLLocation, regionRadius: CLLocationDistance) {
+    public func centerToAnnotation(_ annotation: BMAnnotation, regionRadius: Double) {
         let coordinateRegion = MKCoordinateRegion(
-            center: location.coordinate,
+            center: annotation.coordinate,
             latitudinalMeters: regionRadius,
             longitudinalMeters: regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
-        print("💥", location, regionRadius)
-    }
-    
-    public func addAnnotation(_ annotation: BMAnnotation) {
-        let annotations = MKPointAnnotation()
-        annotations.title = annotation.title
-        annotations.coordinate = annotation.coordinate
-        mapView.addAnnotation(annotations)
-
-        print("💥", annotation)
-    }
-    
-    public func removeAnnotation(_ annotation: BMAnnotation) {
-        print("💥", annotation)
+        print("💥", annotation, regionRadius)
     }
     
     public func removeAnnotations(_ annotations: [BMAnnotation]) {
-        //mapView.removeAnnotations(annotations)
-        print("💥", annotations)
+        mapView.removeAnnotations(mapView.annotations)
     }
     
-    public func selectMarker(_ marker: BMAnnotation) {
-        print("💥", marker)
-        
-        let camera = MKMapCamera(lookingAtCenter: marker.coordinate, fromDistance: 8, pitch: 0, heading: 0)
-        mapView.setCamera(camera, animated: true)
-         
-        // Find the annotation view for the marker
-        if let annotationView = mapView.view(for: marker.mapToMKAnnotation()) {
-            // Scale the annotation view
-            let scale: CGFloat = 1.5 // Set the desired scale factor
-            annotationView.transform = CGAffineTransform(scaleX: scale, y: scale)
+    //TODO: - separate the logic of animating the camera and scaling the annotation
+    public func animateToAnnotation(_ annotation: BMAnnotation, zoomLevel: Double? = nil, animated: Bool = true) {
+        let camera: MKMapCamera
+        if let zoomLevel = zoomLevel {
+            let currentZoom = getZoomLevel()
+            let altitude = pow(2, currentZoom - zoomLevel) * (Double(mapView.bounds.size.width) / 2)
+            camera = MKMapCamera(lookingAtCenter: annotation.coordinate, fromDistance: altitude, pitch: 0, heading: 0)
         } else {
-          debugPrint("🧨: = Marker Not Found On Map")
+            camera = MKMapCamera(lookingAtCenter: annotation.coordinate, fromDistance: mapView.camera.altitude, pitch: 0, heading: 0)
+        }
+        mapView.setCamera(camera, animated: animated)
+    }
+    
+    public func resetAnnotationScaling() {
+        if let lastAnnotationView = lastSelectedAnnotationView {
+            if #available(iOS 14.0, *) {
+                lastAnnotationView.zPriority = .min
+            }
+            //            lastAnnotationView.layer.zPosition = 0
+            //            lastAnnotationView.layer.anchorPointZ = 0
+            UIView.animate(withDuration: 0.1) {
+                lastAnnotationView.transform = CGAffineTransform.identity
+            }
         }
     }
     
-    public func animateToCoordinate(_ coordinate: CLLocationCoordinate2D, withZoomLevel zoomLevel: Double) {
-        print("💥", coordinate, zoomLevel)
+    public func scaleAnnotation(_ annotation: BMAnnotation, selectedScale: CGFloat = 1.5) {
+        let selectedAnnotation =  mapView.annotations.last(where: { $0.coordinate == annotation.coordinate })
+        
+        if let selectedAnnotation = selectedAnnotation, let selectedAnnotationView = mapView.view(for: selectedAnnotation) , selectedAnnotationView !=  lastSelectedAnnotationView {
+            resetAnnotationScaling()
+            let transform = CGAffineTransform(scaleX: selectedScale, y: selectedScale)
+            
+            UIView.animate(withDuration: 0.2) {
+                selectedAnnotationView.transform = transform
+            }
+            
+            if #available(iOS 14.0, *) {
+                selectedAnnotationView.zPriority = .max
+            }
+            //            lastSelectedAnnotationView?.layer.zPosition = 1
+            
+            lastSelectedAnnotationView = selectedAnnotationView
+            
+        } else {
+            print("💥 -  can't find location")
+        }
     }
     
-    public func drawAnnotations(_ annotations: [MKAnnotation]) {
-        print("💥",annotations)
-    }
-    
-    public func fitAnnotationsOnScreen() {
-        print("💥")
+    public func fitAnnotationsInTheScreen(_ annotations: [BMAnnotation], edgePadding: UIEdgeInsets = UIEdgeInsets(top: 50, left: 16, bottom: 50, right: 16)) {
+        addAnnotations(annotations)
+        var zoomRect = MKMapRect.null
+        for branch in annotations {
+            let annotationPoint = MKMapPoint(branch.coordinate)
+            let pointRect = MKMapRect(x: annotationPoint.x, y: annotationPoint.y, width: 0.1, height: 0.1)
+            zoomRect = zoomRect.union(pointRect)
+        }
+        mapView.setVisibleMapRect(zoomRect, edgePadding: edgePadding, animated: true)
     }
     
     public func setDefaultPinIcon(with icon: UIImage) {
@@ -166,6 +197,24 @@ extension BMMapView: BMMapInputType {
     }
 }
 
+// MARK: - Private Handlers
+private extension BMMapView {
+    
+    func addAnnotation(_ annotation: BMAnnotation) {
+        let annotations = MKPointAnnotation()
+        mapView.clearsContextBeforeDrawing = true
+        annotations.title = annotation.title
+        annotations.coordinate = annotation.coordinate
+        mapView.addAnnotation(annotations)
+    }
+    
+    func getZoomLevel() -> Double {
+        let camera = mapView.camera
+        let altitude = camera.altitude
+        let zoomLevel = log2(360 * ((Double(mapView.bounds.size.width) / 256) / altitude))
+        return zoomLevel
+    }
+}
 
 final class AnnotationButton: UIButton {
     var annotation: MKPointAnnotation?
